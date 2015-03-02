7fa28a8347a24ad0ace5aa5a731ac183b66c591e
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-03-02 13:10:49 +0300

    ref/tests

---
+++ verification/src/tests.py
@@ -0,0 +1,29 @@
+TESTS = {
+    "Basics": [
+        {"input": "b1-d5", "answer": 2, "explanation": [[1, 2], [1, 2]]},
+        {"input": "a6-b8", "answer": 1, "explanation": [[1, 2]]},
+        {"input": "h1-g2", "answer": 4, "explanation": [[-2, 1], [-2, 1], [2, 1], [1, -2]]},
+        {"input": "h8-d7", "answer": 3, "explanation": [[-1, -2], [-1, 2], [-2, -1]]},
+        {"input": "a1-h8", "answer": 6, "explanation": [[1, 2], [-1, 2], [1, 2], [2, 1], [2, -1], [2, 1]]},
+        {"input": "b1-e8", "answer": 4, "explanation": [[-1, 2], [1, 2], [1, 2], [2, 1]]},
+        {"input": "e2-e3", "answer": 3, "explanation": [[-2, 1], [1, 2], [1, -2]]},
+        {"input": "d5-b1", "answer": 2, "explanation": [[-1, -2], [-1, -2]]},
+        {"input": "a4-h4", "answer": 5, "explanation": [[1, 2], [1, 2], [2, -1], [2, -1], [1, -2]]},
+        {"input": "c3-d5", "answer": 1, "explanation": [[1, 2]]},
+        {"input": "b2-d5", "answer": 3, "explanation": [[-1, 2], [1, 2], [2, -1]]},
+        {"input": "a5-b8", "answer": 2, "explanation": [[2, 1], [-1, 2]]},
+
+        {"input": "b2-a1", "answer": 4, "explanation": [[2, 1], [-1, 2], [-1, -2], [-1, -2]]},
+        {"input": "g7-h8", "answer": 4, "explanation": [[-1, -2], [-1, 2], [2, -1], [1, 2]]},
+
+
+        {"input": "h3-g2", "answer": 2, "explanation": [[-2, 1], [1, -2]]},
+        {"input": "h4-d7", "answer": 3, "explanation": [[-1, 2], [-1, 2], [-2, -1]]},
+        {"input": "a2-h8", "answer": 5, "explanation": [[1, 2], [1, 2], [1, 2], [2, -1], [2, 1]]},
+        {"input": "b3-e8", "answer": 4, "explanation": [[-1, 2], [1, 2], [2, -1], [1, 2]]},
+        {"input": "e1-e3", "answer": 2, "explanation": [[-2, 1], [2, 1]]},
+        {"input": "d7-b1", "answer": 4, "explanation": [[2, -1], [-1, -2], [-1, -2], [-2, -1]]},
+        {"input": "a2-h4", "answer": 5, "explanation": [[1, 2], [1, 2], [2, 1], [2, -1], [1, -2]]},
+        {"input": "c2-d5", "answer": 2, "explanation": [[-1, 2], [2, 1]]}
+    ]
+}
