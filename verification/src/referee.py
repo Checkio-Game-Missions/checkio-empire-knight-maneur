fa111f78a0f267ccf473c2b65fdbf5443816ddbd
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-03-25 10:40:32 +0300

    refactors

--- verification/src/referee.py
+++ verification/src/referee.py
@@ -10,9 +10,9 @@ class Referee(RefereeCodeGolf):
     TESTS = TESTS
     DEFAULT_MAX_CODE_LENGTH = 250
     BASE_POINTS = 25
-    EXECUTABLE_PATH = settings.EXECUTABLE_PATH
-    CURRENT_ENV = settings_env.CURRENT_ENV
-    FUNCTION_NAME = "golf"
+    ENVIRONMENTS = settings_env.ENVIRONMENTS
+
+    DEFAULT_FUNCTION_NAME = "golf"
     ENV_COVERCODE = {
         "python_2": covercodes.py_2_str,
         "python_3": None,
